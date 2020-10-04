9ba5e32472ae189ce9f624881996824e1a603c1c
mitwhit <umnov.dm@gmail.com>
2020-10-04 21:29:17 +0400

    ScheduleAdapter.onBindViewHolder implemented

--- app/src/main/java/com/demo/fitnessclubapptest/adapters/ScheduleAdapter.java
+++ app/src/main/java/com/demo/fitnessclubapptest/adapters/ScheduleAdapter.java
@@ -9,8 +9,19 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.demo.fitnessclubapptest.R;
+import com.demo.fitnessclubapptest.data.Lesson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder> {
+
+    private List<Lesson> lessons = new ArrayList<>();
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+        notifyDataSetChanged();
+    }
 
     @NonNull
     @Override
@@ -21,12 +32,25 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.Schedu
 
     @Override
     public void onBindViewHolder(@NonNull ScheduleViewHolder holder, int position) {
-        // TODO
+        Lesson lesson = lessons.get(position);
+        String name = lesson.getName();
+        String time = lesson.getStartTime() + " - " + lesson.getEndTime();
+        String teacher = lesson.getTeacher();
+        String place = lesson.getPlace();
+        String description = lesson.getDescription();
+        int weekDay = lesson.getWeekDay();
+
+        holder.textViewName.setText(name);
+        holder.textViewTime.setText(time);
+        holder.textViewTeacher.setText(teacher);
+        holder.textViewPlace.setText(place);
+        holder.textViewDescription.setText(description);
+        holder.textViewDay.setText(String.valueOf(weekDay));
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return lessons.size();
     }
 
     public class ScheduleViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/demo/fitnessclubapptest/data/Lesson.java
+++ app/src/main/java/com/demo/fitnessclubapptest/data/Lesson.java
@@ -8,4 +8,70 @@ public class Lesson {
     private String place;
     private String description;
     private int weekDay;
+
+    public Lesson(String name, String startTime, String endTime, String teacher, String place, String description, int weekDay) {
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.teacher = teacher;
+        this.place = place;
+        this.description = description;
+        this.weekDay = weekDay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getWeekDay() {
+        return weekDay;
+    }
+
+    public void setWeekDay(int weekDay) {
+        this.weekDay = weekDay;
+    }
 }
